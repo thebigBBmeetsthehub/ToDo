99d20b4aaae0841af4ac0c81d2a62d3326036002
Briar Bryant <ceoofydying@gmail.com>
2024-07-30 10:13:37 -0700

    to do list app

---
+++ ToDo/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDo
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
